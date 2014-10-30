fa6bfb7b2f2786633e801dafb674dcdc30f155a2
muraikenta <muraikenta0507@gmail.com>
2014-10-31 04:43:28 +0900

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -20,15 +20,6 @@ ActiveRecord::Schema.define(version: 20141029115345) do
     t.datetime "updated_at"
   end
 
-  create_table "microposts", force: true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
-
   create_table "relationships", force: true do |t|
     t.integer  "follower_id"
     t.integer  "following_id"
